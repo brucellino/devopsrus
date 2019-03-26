18ca2c5e2c4104e3715b759aedf2cd0dccd269d4
Bruce Becker <brucellino@gmail.com>
2019-03-26 13:51:36 +0100

    Add ansible playbook

---
+++ Terraform/aws.tf
@@ -0,0 +1,32 @@
+variable aws_access_key {}
+variable aws_secret_key {}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "eu-west-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-18.10-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "jenkins" {
+  tags = {
+    Name = "jenkins"
+  }
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+}
